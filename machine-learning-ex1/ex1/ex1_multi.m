d588cf17b0cb6fecfe6c9045dcd7d77a085f0cae
Junius <luo.junius@gmail.com>
2019-02-08 14:42:54 -0800

    ex1 multi variables

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -53,20 +53,21 @@ fprintf('Normalizing Features ...\n');
 
 % Add intercept term to X
 X = [ones(m, 1) X];
-
+disp(X(1:10, :));
+pause;
 
 %% ================ Part 2: Gradient Descent ================
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -81,11 +82,51 @@ X = [ones(m, 1) X];
 
 fprintf('Running gradient descent ...\n');
 
+% == experiments: choose a bad alpha value ==
+%fprintf('== gradient descent experiments ==\n');
+%num_iters = 100;
+
+% Choose some alpha value
+% Init Theta and Run Gradient Descent
+%alpha = 0.1;
+%theta = zeros(3, 1);
+%[theta1, J1] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+%alpha = 0.3;
+%theta = zeros(3, 1);
+%[theta2, J2] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+%alpha = 1;
+%theta = zeros(3, 1);
+%[theta3, J3] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+
+% Plot the convergence graph
+%figure;
+%plot(1:numel(J1), J1, 'b');
+%hold on;
+%plot(1:numel(J2), J2, 'r');
+%plot(1:numel(J3), J3, 'k');
+%xlabel('Number of iterations');
+%ylabel('Cost J');
+
+% Display gradient descent's result
+%fprintf('Theta computed from gradient descent: \n');
+%fprintf('theta1 %f \n', theta1);
+%fprintf('theta2 %f \n', theta2);
+%fprintf('theta3 %f \n', theta3);
+%fprintf('\n');
+
+%fprintf('== gradient descent experiments end ==\n');
+%pause;
+% == experiments end ==
+
+
 % Choose some alpha value
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
+% theta = zeros(size(X, 2), 1);
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -104,7 +145,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = theta(1) + theta(2) * 1650 + theta(3) * 3; % You should change this
 
 
 % ============================================================
@@ -120,12 +161,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -149,7 +190,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta(1) + theta(2) * 1650 + theta(3) * 3; % You should change this
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -7,31 +7,35 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+%polyX = [X(:, 1:2) X(:, 3) .^ 2];
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
 
-
-
-
-
-
-
-
+    h = X * theta;
+    % theta = theta - alpha * (sum((h - y) .* X))' / m;
+    theta = theta - alpha * (X' * (h - y)) / m;
+    %h = polyX * theta;
+    %theta = theta - alpha * (polyX' * (polyX * theta - y)) / m;
 
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
+    if (rem(iter, 100) == 0)
+        disp('iter:'), disp(iter), disp('theta:'), disp(theta), disp('J:'), disp(J_history(iter));
+        %fprintf('iter=%d, theta=%f, J=%f\n', iter, theta, J_history(iter));
+    endif
 end
 
 end
