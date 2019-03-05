12fac1b37268382653f40c122c448dd629cb53d4
Junius <luo.junius@gmail.com>
2019-03-04 23:43:13 -0800

    gaussian kernel, tuning, email features

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,29 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+e = 100.0;
 
-
-
-
+for C_i = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+  for sigma_i = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    model= svmTrain(X, y, C_i, @(x1, x2) gaussianKernel(x1, x2, sigma_i));
+    p = svmPredict(model, Xval);
+    e_i = mean(double(p ~= yval));
+    if (e_i < e)
+      e = e_i;
+      C = C_i;
+      sigma = sigma_i;
+    endif
+    %fprintf('c_i=%f sigma_i=%f currError=%f, c=%f sigma=%f meanError=%f', C_i, sigma_i, e_i, C, sigma, e);
+  end
+end
 
 
 % =========================================================================
